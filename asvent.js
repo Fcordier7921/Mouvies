250da3156be4999b3fed357ec3888ec064ef2bb1
Fcordier7921 <f.cordier@codeur.online>
2020-12-09 15:40:05 +0100

    generation grace a une api

--- asvent.js
+++ asvent.js
@@ -1,26 +1,25 @@
+let Upcoming = ajaxGet("https://api.themoviedb.org/3/list/1?api_key=22a304eaf99b49c9b1427583f1654aba&languages=fr-fr");
+let AllMovies = Upcoming['items'];
 
-Upcoming=ajaxGet("https://api.themoviedb.org/3/list/1?api_key=22a304eaf99b49c9b1427583f1654aba&languages=fr-fr");
-AllMovies=Upcoming['items'];
 
-function ajaxGet(url)
-{
-var data;
-var req = new XMLHttpRequest();
-req.open("GET", url, false);
-req.addEventListener("load", function () {
-if (req.status >= 200 && req.status < 400) {
-data=JSON.parse(req.responseText);
-// console.log(req.responseText);
+function ajaxGet(url) {
+    var data;
+    var req = new XMLHttpRequest();
+    req.open("GET", url, false);
+    req.addEventListener("load", function () {
+        if (req.status >= 200 && req.status < 400) {
+            data = JSON.parse(req.responseText);
+            
 
-} else {
-console.error(req.status + " " + req.statusText + " " + url);
-}
-});
-req.addEventListener("error", function () {
-console.error("Erreur réseau avec l'URL " + url);
-});
-req.send();
-return data;
+        } else {
+            console.error(req.status + " " + req.statusText + " " + url);
+        }
+    });
+    req.addEventListener("error", function () {
+        console.error("Erreur réseau avec l'URL " + url);
+    });
+    req.send();
+    return data;
 }
 
 // const mouviResquy= Array();
@@ -31,7 +30,7 @@ return data;
 //             res.json().then(data => {
 //                  mouviResquy.push(data.items);
 //                 console.log(data.items);
-                
+
 //             })
 //         } else {
 //             console.log("ERREUR");
@@ -41,43 +40,73 @@ return data;
 //     function ajaxGet(url)
 // {
 
-    
+
 // console.log(AllMovies)
-
-
-    
-    
-    for (let i = 0; i < AllMovies.length; i++) {
-        
-        let nuberMovies= AllMovies[i];
-        let titleMovies=nuberMovies.original_title;
-        let originalLanguageMovies=nuberMovies.original_language;
-        let poster_pathMovies=nuberMovies.poster_path;
-        var movis = document.createElement('div');
-        movis.className = "movie";
-        movis.style= "widht: 100%; min-height: 50vh";
-        movis.innerHTML ='<h4>'+titleMovies+'</h4><br><h6>'+originalLanguageMovies+'</h6><br><div><img scr="https://image.tmdb.org/t/p/w500'+poster_pathMovies+'"></div>';
-        console.log(movis)
-        // document.getElementById('movieGeneral').appendChild(movis);
-    };
+let nuberMovies;
+let titleMovies;
+let poster_pathMovies;
+let movis
 
 
 
-    var callBackGetSuccess= function(data){
-    console.log("donnees api", data)
-    var element=document.getElementById('meteo');
-    element.innerHTML="the temperature is "+data.main.temp+"°K<br> a wind of "+data.wind.speed+" km/h";
+for (let i = 0; i < AllMovies.length; i++) {
+
+    nuberMovies = AllMovies[i];
+    titleMovies = nuberMovies.original_title;
+    poster_pathMovies = nuberMovies.poster_path;
+    movis = document.createElement('div');
+    movis.className = "movie";
+    movis.style = "widht: 100%; min-height: 30vh";
+    movis.innerHTML ='<div class="movie-image">  <a href="#" data-toggle="modal" data-target="#m" onclick="myFunction()"><img src="https://image.tmdb.org/t/p/w500' + poster_pathMovies + '" alt="" /></a> <span class="play"><span class="name">'+ titleMovies +'</span></span></div>';
+    // console.log(movis);
+document.getElementById('movieGeneral').appendChild(movis);
+
+};
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+function myFunction(id) {
+    let Upcoming = ajaxGet("https://api.themoviedb.org/3/movie/531219/credits?api_key=22a304eaf99b49c9b1427583f1654aba&languages=fr-fr");
+    console.log(Upcoming);
+  modal.style.display = "block";
 }
 
-function buttonClickGET(){
-    let url="http://api.openweathermap.org/data/2.5/weather?q=Paris&appid=44c83eccb126388f53495b2fa7e02165";
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
 
-    $.get(url, callBackGetSuccess).done(function(){
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
-    }).fail(function(){
-        alert("error");
-    })
-    .always(function(){
 
-    });
+
+
+var callBackGetSuccess = function (data) {
+    console.log("donnees api", data)
+    var element = document.getElementById('meteo');
+    element.innerHTML = "the temperature is " + data.main.temp + "°K<br> a wind of " + data.wind.speed + " km/h";
+}
+
+function buttonClickGET() {
+    let url = "http://api.openweathermap.org/data/2.5/weather?q=Paris&appid=44c83eccb126388f53495b2fa7e02165";
+
+    $.get(url, callBackGetSuccess).done(function () {
+
+        }).fail(function () {
+            alert("error");
+        })
+        .always(function () {
+
+        });
 }

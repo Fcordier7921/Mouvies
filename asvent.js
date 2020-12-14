af611922243f9c49b56e8b9d452ad57d97b6ec63
Fcordier7921 <f.cordier@codeur.online>
2020-12-14 14:06:25 +0100

    modifier la modal rajout d'un titre

--- asvent.js
+++ asvent.js
@@ -183,33 +183,3 @@ function buttonClickGET() {
 
         });
 }
-
-
-
-
-
-
-//     let Recommendation = UpRecommendation['results'];
-//     let reviews = UpReviews['results'];
-//     //les variable pour l'entaite de la modal
-//     let originalLanguage = nuberMovies.original_language;
-//     let originalTitle = nuberMovies.original_title;
-//     let overviewMovies = nuberMovies.overview;
-//     //recuperation du titre du fil et dela photo
-//     let movisModal = document.createElement('div');
-//     movisModal.className = "movieModal";
-//     movisModal.style = "widht: 20%; min-height: 20%";
-//     movisModal.innerHTML = '<div class="container-fluid"><div class="row"><div><img src="https://image.tmdb.org/t/p/w500' + poster_pathMovies + '" alt="' + titleMovies + '" /></div><div><h2>' + titleMovies + '</h2><h4>Title original : ' + originalTitle + '</h4><h4>Language original : ' + originalLanguage + '</h4></div></div></div><div class="container-fluid"><p>description : <br>'+overviewMovies+'</p></div>';
-//     console.log(movisModal);
-//     document.getElementById('myModal').appendChild(movisModal);
-
-//     // for (i = 0; i < 5; i++) {
-//     //     let nuberActors = Actor[i];
-//     //     titleMovies = nuberActors.title;
-//     //     poster_pathMovies = nuberActors.poster_path;
-//     //     let movisActors = document.createElement('div');
-//     //     movisActors.className = "movie";
-//     //     movisActors.style = "widht: 100%; min-height: 30vh";
-//     //     movisActors.innerHTML = '<div class="movie-image ">  <a href="#" data-toggle="modal" data-target="#m" onclick="myFunction(' + id + ')"><img src="https://image.tmdb.org/t/p/w500' + poster_pathMovies + '" alt="" /></a> <span class="play"><span class="name">' + titleMovies + '</span></span></div>';
-//     //     // console.log(movis);
-//     //     document.getElementById('movieGeneral').appendChild(movisActors);
